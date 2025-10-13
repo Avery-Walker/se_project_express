5c1db202b61913c4ca26257e20124ca4c683bd59
AveryWalker <aswalk2023@gmail.com>
2025-10-13 14:33:44 -0500

    corrected file pathing and corrected spelling when calling router.use

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ const router = require("express").Router();
 
 const userRouter = require("./users");
 
-route.use("./users", userRouter);
+router.use("/users", userRouter);
 
 module.exports = router;
