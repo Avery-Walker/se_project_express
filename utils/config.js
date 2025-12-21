311082bcc5d330747823389d535ba054ec9de322
AveryWalker <aswalk2023@gmail.com>
2025-12-21 16:19:14 -0600

    added env secret support

--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,6 @@
-const { JWT_SECRET = "super-strong-secret" } = process.env;
+const { JWT_SECRET = "super-strong-secret", NODE_ENV } = process.env;
 
 module.exports = {
   JWT_SECRET,
+  NODE_ENV,
 };
